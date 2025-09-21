29112c9f205f634e99b39cbcb5ac5d67aa0115a3
sophia <sophiawang8509@gmail.com>
2025-09-21 16:27:56 -0400

    servo + multiple paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/NextFTCPedroAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/NextFTCPedroAuto.java
@@ -9,37 +9,52 @@ import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.extensions.pedro.FollowPath;
-
+import dev.nextftc.hardware.positionable.SetPosition;
 import static dev.nextftc.extensions.pedro.PedroComponent.follower;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.impl.ServoEx;
+
 @Autonomous(name = "NextFTC Pedro Auto")
 public class NextFTCPedroAuto extends NextFTCOpMode {
-
+    private ServoEx servo = new ServoEx("Servo");
     // Define poses
     private static final Pose startPose = new Pose(72, 72, Math.toRadians(0));
     private static final Pose scorePose = new Pose(100, 100, Math.toRadians(0));
+    private static final Pose endPose = new Pose(30, 100);
     private PathChain scorePreload;
+    private PathChain newPath;
 
     {
         addComponents(
-                new PedroComponent(Constants::createFollower)
+                new PedroComponent(Constants::createFollower),
+                BulkReadComponent.INSTANCE
         );
     }
-
+    private Command moveServo = new SetPosition(servo, 0.2).requires(this);
     private Command autonomousRoutine(){
-        return new SequentialGroup(
-                new FollowPath(scorePreload)
+        return new ParallelGroup(
+                new SequentialGroup(
+                    new FollowPath(scorePreload),
+                    new FollowPath(newPath)
+                ),
+                moveServo
         );
     }
     public void buildPaths() {
         scorePreload = follower().pathBuilder()
                 .addPath(new BezierLine(startPose, scorePose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading())
+                .build();
+        newPath = follower().pathBuilder()
+                .addPath(new BezierLine(scorePose, endPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading())
                 .build();
     }
