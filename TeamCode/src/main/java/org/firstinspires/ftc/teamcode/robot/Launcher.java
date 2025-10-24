ee7f8be8cfadd6a5e050e201e74bb755e73a574a
sophia <sophiawang8509@gmail.com>
2025-10-24 19:08:43 -0400

    launcher subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Launcher.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import static dev.nextftc.extensions.pedro.PedroComponent.follower;
+
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.extensions.pedro.FollowPath;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.positionable.SetPosition;
+import dev.nextftc.hardware.powerable.SetPower;
+
+public class Launcher implements Subsystem {
+
+    public static final Launcher INSTANCE = new Launcher();
+    private Launcher() {}
+
+    private MotorEx motor1 = new MotorEx("LeftMotor");
+    private MotorEx motor2 = new MotorEx("RightMotor");
+
+    public Command inwards() {
+        return new ParallelGroup(
+                new SetPower(motor1, 1),
+                new SetPower(motor2, 1)
+        );
+    }
+    public Command outward() {
+        return new ParallelGroup(
+                new SetPower(motor1, -1),
+                new SetPower(motor2, -1)
+        );
+    }
+}
