58252f8d8e14e73884fcf103cbb9e1cf286479c1
axion66 <ssongjinseob@gmail.com>
2025-12-10 16:30:10 -0500

    add Angle Alignment func (returning angle; does not turn) on Limelight.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Limelight.java
@@ -7,8 +7,16 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import org.firstinspires.ftc.robotcore.internal.hardware.android.GpioPin;
 
 import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelDeadlineGroup;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+
+import dev.nextftc.ftc.Gamepads;
+
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.ftc.ActiveOpMode;
+
 
 import java.util.List;
 
@@ -20,6 +28,7 @@ public class Limelight implements Subsystem {
     private Limelight3A ll;
     private double distanceFromLimelightToGoal;
     private double goalVelocity;
+    private double angleForAlignment;
 
     @Override
     public void initialize() {
@@ -28,6 +37,7 @@ public class Limelight implements Subsystem {
         ll.start();
         ll.pipelineSwitch(1);
     }
+
 
     public double[] calculateLaunchPower() {
         LLResult result = ll.getLatestResult();
@@ -40,11 +50,40 @@ public class Limelight implements Subsystem {
                     double z = fiducial.getRobotPoseTargetSpace().getPosition().z;
                     distanceFromLimelightToGoal = Math.sqrt(x * x + z * z);
                     goalVelocity = 269.70662 * distanceFromLimelightToGoal + 915.21596 + 70;
+
+
+                    // angle alignment
+                    double camera_angle_to_target = fiducial.getTargetXDegrees();
+                    driverControlled.cancel();
+                    Command turn_ = turns(anglefactor * camera_angle_to_target);
+                    telemetry.update();
+                    turn_.schedule();
+
                     return new double[]{distanceFromLimelightToGoal, goalVelocity};
                 }
             }
         }
         return new double[]{0,0};
+    }
+
+    public double calculateAlignmentAngle() {
+        LLResult result = ll.getLatestResult();
+        if (result != null && result.isValid()) {
+            List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
+            for (LLResultTypes.FiducialResult fiducial : fiducials) {
+                int id = fiducial.getFiducialId();
+                if (id == 20 || id == 24) {
+
+                    angleForAlignment = fiducial.getTargetXDegrees(); 
+                    //Command turn_ = turns(anglefactor * camera_angle_to_target);
+                    //telemetry.update();
+                    //turn_.schedule();
+
+                    return new angleForAlignment;
+                }
+            }
+        }
+        return new 0;
     }
 
 }
