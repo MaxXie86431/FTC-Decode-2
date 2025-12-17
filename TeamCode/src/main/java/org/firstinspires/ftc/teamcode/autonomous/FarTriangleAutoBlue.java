99c91245de75a8cf747a7c445d635499dcff600b
sophia <sophiawang8509@gmail.com>
2025-12-17 16:47:10 -0500

    far auto code test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarTriangleAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarTriangleAutoBlue.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
@@ -16,6 +17,7 @@ import dev.nextftc.extensions.pedro.FollowPath;
 
 import static dev.nextftc.extensions.pedro.PedroComponent.follower;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.robot.Flywheel;
 import org.firstinspires.ftc.teamcode.robot.Intake;
@@ -28,78 +30,126 @@ import dev.nextftc.ftc.components.BulkReadComponent;
 @Configurable
 @Autonomous(name = "Far Triangle Auto Blue")
 public class FarTriangleAutoBlue extends NextFTCOpMode { // Define poses
-    private static final Pose startPose = new Pose(56, 8, Math.toRadians(110));
-    private static final Pose middleRowStartPose = new Pose(50, 60, Math.toRadians(180));
-    private static final Pose middleRowEndPose = new Pose(20, 60, Math.toRadians(180));
-    private static final Pose bottomRowStartPose = new Pose(45, 35, Math.toRadians(110));
-    private static final Pose bottomRowEndPose = new Pose(20, 35, Math.toRadians(180));
-    private static final Pose endPose = new Pose(38.5, 34, Math.toRadians(180));
-    public static double offset = 20;
+        private static final Pose startPose = new Pose(56, 8, Math.toRadians(110));
+        private static final Pose topRowStartPose = new Pose(50, 84.35, Math.toRadians(180));
+        private static final Pose topRowEndPose = new Pose(20, 84.35, Math.toRadians(180));
+        private static final Pose middleRowStartPose = new Pose(50, 60, Math.toRadians(180));
+        private static final Pose middleRowEndPose = new Pose(20, 60, Math.toRadians(180));
+        private static final Pose bottomRowStartPose = new Pose(45, 35, Math.toRadians(180));
+        private static final Pose bottomRowEndPose = new Pose(20, 35, Math.toRadians(180));
+        private static Pose frontLaunchPose = new Pose(59, 84.3, Math.toRadians(140));
+        private static final Pose endPose = new Pose(38.5, 34, Math.toRadians(180));
+        public static double offset = 20;
+        private PathChain initialToBottomStart;
+        private PathChain middleRowPath;
 
-    private PathChain initialToBottomStart;
-    private PathChain middleRowPath;
+        private PathChain bottomRowPath;
+        private PathChain topRowPath;
+        private PathChain outtaTheWay;
 
-    private PathChain bottomRowPath;
-    private PathChain outtaTheWayPath;
-    public static double delay = 2;
-    public static double betweenBallsDelay = 0.2;
-    public static double intermediateDelay = 1;
-    public static double afterBallsDelay = 3;
-    public static double power = 1;
+        private Telemetry debugTelemetry;
+        {
+            addComponents(
+                    new PedroComponent(Constants::createFollower),
+                    new SubsystemComponent(Launcher.INSTANCE),
+                    new SubsystemComponent(Intake.INSTANCE),
+                    new SubsystemComponent(Intermediate.INSTANCE),
+                    BulkReadComponent.INSTANCE
+            );
+        }
+        //open (0.2) is logo on left closed (0) is logo on right
+        //private Command moveServo = new SetPosition(servo, 0.2).requires(this);
+        private Command autonomousRoutine(){
+            return new SequentialGroup(
+                    Flywheel.INSTANCE.constantShot(1800),
+                    new Delay(3),
+                    Flywheel.INSTANCE.shutdown(),
+                    new FollowPath(initialToBottomStart),
+                    new FollowPath(bottomRowPath),
+                    Flywheel.INSTANCE.constantShot(1800),
+                    new Delay(3),
+                    Flywheel.INSTANCE.shutdown(),
+                    new FollowPath(middleRowPath),
+                    Flywheel.INSTANCE.constantShot(1500),
+                    new Delay(3),
+                    Flywheel.INSTANCE.shutdown(),
+                    new FollowPath(topRowPath),
+                    Flywheel.INSTANCE.constantShot(1500),
+                    new Delay(3),
+                    Flywheel.INSTANCE.shutdown(),
+                    new FollowPath(outtaTheWay)
+            );
+        }
+        public void buildPaths() {
+            initialToBottomStart = follower().pathBuilder()
+                    .addPath(new BezierLine(startPose, bottomRowStartPose))
+                    .setLinearHeadingInterpolation(startPose.getHeading(), bottomRowStartPose.getHeading())
+                    .build();
+            topRowPath = follower().pathBuilder()
+                    .addPath(new BezierLine(startPose, topRowStartPose))
+                    .addParametricCallback(Constants.completion, () -> {
+                        debugTelemetry.addData("CALLBACK", "topRowPath inward triggered");
+                        debugTelemetry.update();
+                        new InstantCommand(() -> Intake.INSTANCE.inward()).schedule();
+                    })
+                    .addPath(new BezierLine(topRowStartPose, topRowEndPose))
+                    .addPath(new BezierLine(topRowEndPose, frontLaunchPose))
+                    .addParametricCallback(0.2, () -> {
+                        debugTelemetry.addData("CALLBACK", "topRowPath stop triggered");
+                        debugTelemetry.update();
+                        Intake.INSTANCE.stop().schedule();
+                    })
+                    .setLinearHeadingInterpolation(topRowEndPose.getHeading(), frontLaunchPose.getHeading())
+                    .build();
+            middleRowPath = follower().pathBuilder()
+                    .addPath(new BezierLine(startPose, middleRowStartPose))
+                    .addParametricCallback(Constants.completion, () -> {
+                        debugTelemetry.addData("CALLBACK", "middleRowPath inward triggered");
+                        debugTelemetry.update();
+                        new InstantCommand(() -> Intake.INSTANCE.inward()).schedule();
+                    })
+                    .addPath(new BezierLine(middleRowStartPose, middleRowEndPose))
+                    .addPath(new BezierLine(middleRowEndPose, startPose))
+                    .addParametricCallback(0.2, () -> {
+                        debugTelemetry.addData("CALLBACK", "middleRowPath stop triggered");
+                        debugTelemetry.update();
+                        Intake.INSTANCE.stop().schedule();
+                    })
+                    .setLinearHeadingInterpolation(middleRowEndPose.getHeading(), startPose.getHeading())
+                    .build();
+            bottomRowPath = follower().pathBuilder()
+                    .addPath(new BezierLine(bottomRowStartPose, bottomRowEndPose))
+                    .addParametricCallback(0, () -> {
+                        debugTelemetry.addData("CALLBACK", "bottomRowPath inward triggered");
+                        debugTelemetry.update();
+                        Intake.INSTANCE.inward().schedule();
+                    })
+                    .addPath(new BezierLine(bottomRowEndPose, startPose))
+                    .addParametricCallback(0.2, () -> {
+                        debugTelemetry.addData("CALLBACK", "bottomRowPath stop triggered");
+                        debugTelemetry.update();
+                        Intake.INSTANCE.stop().schedule();
+                    })
+                    .setLinearHeadingInterpolation(bottomRowEndPose.getHeading(), startPose.getHeading())
+                    .build();
+            outtaTheWay = follower().pathBuilder()
+                    .addPath(new BezierLine(frontLaunchPose, middleRowStartPose))
+                    .setLinearHeadingInterpolation(frontLaunchPose.getHeading(), middleRowStartPose.getHeading())
+                    .build();
+        }
 
-    {
-        addComponents(
-                new PedroComponent(Constants::createFollower),
-                new SubsystemComponent(Intake.INSTANCE, Intermediate.INSTANCE, Flywheel.INSTANCE, Limelight.INSTANCE),
-                BulkReadComponent.INSTANCE
-        );
+        @Override
+        public void onInit() {
+            // Initialize the follower with your constants
+            follower().setStartingPose(startPose);
+            debugTelemetry = telemetry;
+            buildPaths();
+        }
+
+
+        @Override
+        public void onStartButtonPressed() {
+            autonomousRoutine().schedule();
+        }
+
     }
-    //open (0.2) is logo on left closed (0) is logo on right
-    //private Command moveServo = new SetPosition(servo, 0.2).requires(this);
-    private Command autonomousRoutine(){
-        return new SequentialGroup(
-                Flywheel.INSTANCE.shootOut(),
-                Intake.INSTANCE.rawRoll(),
-                new Delay(5),
-                Intake.INSTANCE.stop(),
-                Flywheel.INSTANCE.shutdown(),
-                new FollowPath(initialToBottomStart)
-
-        );
-    }
-    public void buildPaths() {
-        initialToBottomStart = follower().pathBuilder()
-                .addPath(new BezierLine(startPose, bottomRowStartPose))
-                .setLinearHeadingInterpolation(startPose.getHeading(), bottomRowStartPose.getHeading())
-                .build();
-        middleRowPath = follower().pathBuilder()
-                .addPath(new BezierLine(startPose, middleRowStartPose))
-                .addPath(new BezierLine(middleRowStartPose, middleRowEndPose))
-                .addPath(new BezierLine(middleRowEndPose, startPose))
-                .setLinearHeadingInterpolation(middleRowEndPose.getHeading(), startPose.getHeading())
-                .build();
-        bottomRowPath = follower().pathBuilder()
-                .addPath(new BezierLine(bottomRowStartPose, bottomRowEndPose))
-                .addPath(new BezierLine(bottomRowEndPose, startPose))
-                .setLinearHeadingInterpolation(bottomRowEndPose.getHeading(), startPose.getHeading())
-                .build();
-        outtaTheWayPath = follower().pathBuilder()
-                .addPath(new BezierLine(startPose, endPose))
-                .setLinearHeadingInterpolation(bottomRowStartPose.getHeading(), bottomRowEndPose.getHeading())
-                .build();
-    }
-
-    @Override
-    public void onInit() {
-        // Initialize the follower with your constants
-        follower().setStartingPose(startPose);
-        buildPaths();
-    }
-
-
-    @Override
-    public void onStartButtonPressed() {
-        autonomousRoutine().schedule();
-    }
-
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarTriangleAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/FarTriangleAutoRed.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.extensions.pedro.PedroComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
@@ -16,6 +17,7 @@ import dev.nextftc.extensions.pedro.FollowPath;
 
 import static dev.nextftc.extensions.pedro.PedroComponent.follower;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.robot.Flywheel;
 import org.firstinspires.ftc.teamcode.robot.Intake;
@@ -28,10 +30,14 @@ import dev.nextftc.ftc.components.BulkReadComponent;
 @Autonomous(name = "Far Triangle Auto Red")
 public class FarTriangleAutoRed extends NextFTCOpMode { // Define poses
     private static final Pose startPose = new Pose(88, 8, Math.toRadians(72));
-    private static final Pose middleRowStartPose = new Pose(50, 60, Math.toRadians(180));
-    private static final Pose middleRowEndPose = new Pose(20, 60, Math.toRadians(180));
-    private static final Pose bottomRowStartPose = new Pose(99, 35, Math.toRadians(72));
-    private static final Pose bottomRowEndPose = new Pose(20, 35, Math.toRadians(180));
+    private static final Pose frontLaunchPose = new Pose(85, 85, Math.toRadians(225));
+    private static final Pose topRowStartPose = new Pose(100, 84.35, Math.toRadians(180));
+    private static final Pose topRowEndPose = new Pose(131, 84.35, Math.toRadians(180));
+    private static final Pose middleRowStartPose = new Pose(100, 60, Math.toRadians(180));
+    private static final Pose middleRowEndPose = new Pose(131, 60, Math.toRadians(180));
+    private static final Pose bottomRowStartPose = new Pose(100, 35, Math.toRadians(72));
+    private static final Pose bottomRowEndPose = new Pose(131, 35, Math.toRadians(180));
+
     private static final Pose endPose = new Pose(105.5, 34, Math.toRadians(0));
     public static double offset = 20;
 
@@ -40,7 +46,10 @@ public class FarTriangleAutoRed extends NextFTCOpMode { // Define poses
     private PathChain middleRowPath;
 
     private PathChain bottomRowPath;
-    private PathChain outtaTheWayPath;
+    private PathChain topRowPath;
+    private PathChain outtaTheWay;
+
+    private Telemetry debugTelemetry;
 
     public static double delay = 2;
     public static double betweenBallsDelay = 0.2;
@@ -61,12 +70,23 @@ public class FarTriangleAutoRed extends NextFTCOpMode { // Define poses
     //private Command moveServo = new SetPosition(servo, 0.2).requires(this);
     private Command autonomousRoutine(){
         return new SequentialGroup(
-                Flywheel.INSTANCE.shootOut(),
-                Intake.INSTANCE.rawRoll(),
-                new Delay(5),
-                Intake.INSTANCE.stop(),
+                Flywheel.INSTANCE.constantShot(1800),
+                new Delay(3),
                 Flywheel.INSTANCE.shutdown(),
-                new FollowPath(initialToBottomStart)
+                new FollowPath(initialToBottomStart),
+                new FollowPath(bottomRowPath),
+                Flywheel.INSTANCE.constantShot(1800),
+                new Delay(3),
+                Flywheel.INSTANCE.shutdown(),
+                new FollowPath(middleRowPath),
+                Flywheel.INSTANCE.constantShot(1500),
+                new Delay(3),
+                Flywheel.INSTANCE.shutdown(),
+                new FollowPath(topRowPath),
+                Flywheel.INSTANCE.constantShot(1200),
+                new Delay(3),
+                Flywheel.INSTANCE.shutdown(),
+                new FollowPath(outtaTheWay)
         );
     }
     public void buildPaths() {
@@ -74,20 +94,56 @@ public class FarTriangleAutoRed extends NextFTCOpMode { // Define poses
                 .addPath(new BezierLine(startPose, bottomRowStartPose))
                 .setLinearHeadingInterpolation(startPose.getHeading(), bottomRowStartPose.getHeading())
                 .build();
+        topRowPath = follower().pathBuilder()
+                .addPath(new BezierLine(startPose, topRowStartPose))
+                .addParametricCallback(Constants.completion, () -> {
+                    debugTelemetry.addData("CALLBACK", "topRowPath inward triggered");
+                    debugTelemetry.update();
+                    new InstantCommand(() -> Intake.INSTANCE.inward()).schedule();
+                })
+                .addPath(new BezierLine(topRowStartPose, topRowEndPose))
+                .addPath(new BezierLine(topRowEndPose, frontLaunchPose))
+                .addParametricCallback(0.2, () -> {
+                    debugTelemetry.addData("CALLBACK", "topRowPath stop triggered");
+                    debugTelemetry.update();
+                    Intake.INSTANCE.stop().schedule();
+                })
+                .setLinearHeadingInterpolation(topRowEndPose.getHeading(), frontLaunchPose.getHeading())
+                .build();
         middleRowPath = follower().pathBuilder()
                 .addPath(new BezierLine(startPose, middleRowStartPose))
+                .addParametricCallback(Constants.completion, () -> {
+                    debugTelemetry.addData("CALLBACK", "middleRowPath inward triggered");
+                    debugTelemetry.update();
+                    new InstantCommand(() -> Intake.INSTANCE.inward()).schedule();
+                })
                 .addPath(new BezierLine(middleRowStartPose, middleRowEndPose))
                 .addPath(new BezierLine(middleRowEndPose, startPose))
+                .addParametricCallback(0.2, () -> {
+                    debugTelemetry.addData("CALLBACK", "middleRowPath stop triggered");
+                    debugTelemetry.update();
+                    Intake.INSTANCE.stop().schedule();
+                })
                 .setLinearHeadingInterpolation(middleRowEndPose.getHeading(), startPose.getHeading())
                 .build();
         bottomRowPath = follower().pathBuilder()
                 .addPath(new BezierLine(bottomRowStartPose, bottomRowEndPose))
+                .addParametricCallback(0, () -> {
+                    debugTelemetry.addData("CALLBACK", "bottomRowPath inward triggered");
+                    debugTelemetry.update();
+                    Intake.INSTANCE.inward().schedule();
+                })
                 .addPath(new BezierLine(bottomRowEndPose, startPose))
+                .addParametricCallback(0.2, () -> {
+                    debugTelemetry.addData("CALLBACK", "bottomRowPath stop triggered");
+                    debugTelemetry.update();
+                    Intake.INSTANCE.stop().schedule();
+                })
                 .setLinearHeadingInterpolation(bottomRowEndPose.getHeading(), startPose.getHeading())
                 .build();
-        outtaTheWayPath = follower().pathBuilder()
-                .addPath(new BezierLine(startPose, endPose))
-                .setLinearHeadingInterpolation(bottomRowStartPose.getHeading(), bottomRowEndPose.getHeading())
+        outtaTheWay = follower().pathBuilder()
+                .addPath(new BezierLine(frontLaunchPose, middleRowStartPose))
+                .setLinearHeadingInterpolation(frontLaunchPose.getHeading(), middleRowStartPose.getHeading())
                 .build();
     }
 
@@ -95,6 +151,7 @@ public class FarTriangleAutoRed extends NextFTCOpMode { // Define poses
     public void onInit() {
         // Initialize the follower with your constants
         follower().setStartingPose(startPose);
+        debugTelemetry = telemetry;
         buildPaths();
     }
 
